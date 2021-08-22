c750ce7b322dac217259a190745fdf69784e2afe
Henry Muru Paenga <meringu@gmail.com>
2021-08-23 00:03:28 +1200

    suspense for component loading

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { lazy } from 'react'
+import React, { lazy, Suspense } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import { PouchDB } from 'react-pouchdb'
 
 import Header from './components/Header'
+import Loader from './components/Loader'
 
 const Home = lazy(() => import('./pages/Home'))
 const Library = lazy(() => import('./pages/Library'))
@@ -13,11 +14,13 @@ const App = () => (
   <BrowserRouter>
     <PouchDB name="murushop" auto_compaction={true}>
       <Header />
-      <Switch>
-        <Route path="/edit/:id" component={Edit}/>
-        <Route path="/library" component={Library} />
-        <Route path="/" component={Home} />
-      </Switch>
+      <Suspense fallback={<Loader/>}>
+        <Switch>
+          <Route path="/edit/:id" component={Edit}/>
+          <Route path="/library" component={Library} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </Suspense>
     </PouchDB>
   </BrowserRouter>
 )
